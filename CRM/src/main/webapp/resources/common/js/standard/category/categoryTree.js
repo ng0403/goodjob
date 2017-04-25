ea9c26a36c183e90926bdbbe9e5f4abc349511b2
Core <Core@Core-PC>
2017-04-25 15:42:50 +0900

    상품카테고리

--- CRM/src/main/webapp/resources/common/js/standard/category/categoryTree.js
+++ CRM/src/main/webapp/resources/common/js/standard/category/categoryTree.js
@@ -87,27 +87,36 @@ function cate_checkNodeList(ClassName){
 }
 
 //메뉴상세 정보 적용
-function cateDetailSetting(cate){
+function cateDetailSetting(cate)
+{
 	$('#cate_id').val(cate.cate_id);
 	$('#cate_nm').val(cate.cate_nm);
 	$('#up_cate_id').val(cate.up_cate_id);
 	$('#up_cate_nm').val(cate.up_cate_nm);
 	$('#cate_dtl_cont').val(cate.cate_dtl_cont);
-	if(cate.act_yn == 'Y'){
+	
+	if(cate.act_yn == 'Y')
+	{
 		$('#act_y').prop('checked',true);
 		$('#act_n').prop('checked',false);
-	}else{
+	}
+	else
+	{
 		$('#act_n').prop('checked',true);
 		$('#act_y').prop('checked',false);
 	}
 }
 
 //체크된 노드의 cateID
-function cate_checkedCateIDList(ClassName){
+function cate_checkedCateIDList(ClassName)
+{
 	var list = new Array();
 	var checkList =	$('.'+ClassName);
-	for(var i=0; i<checkList.size(); i++){
-		if($(checkList[i]).is(':checked')){
+	
+	for(var i=0; i<checkList.size(); i++)
+	{
+		if($(checkList[i]).is(':checked'))
+		{
 			list.push($(checkList[i]).val());
 		}
 	}
@@ -115,10 +124,13 @@ function cate_checkedCateIDList(ClassName){
 }
 
 //메뉴이름 여부 확인
-function validatecateName(ctx,cate_name){
+function validatecateName(ctx,cate_name)
+{
 	var count;
 	var obj = new Object();
+	
 	obj.cate_nm = cate_name;
+	
 	var data = JSON.stringify(obj);
 	$.ajaxSettings.traditional = true;
 	$.ajax({
@@ -129,9 +141,12 @@ function validatecateName(ctx,cate_name){
         contentType : 'application/json; charset=UTF-8',	//서버 전송 시 데이터가 JSON 객체
         dataType:'json',
         success: function(result){
-        	if(result == 0){
+        	if(result == 0)
+        	{
         		count = 'false';
-        	}else{
+        	}
+        	else
+        	{
         		count = 'true';
         	}
         },
@@ -143,9 +158,11 @@ function validatecateName(ctx,cate_name){
 }
 
 //메뉴ID에 대한 상세정보
-function getcateDetail(ctx,cate_id){
+function getcateDetail(ctx,cate_id)
+{
 	var list;
 	var data = JSON.stringify(cate_id);
+	
 	$.ajax({
 		url: ctx+'/cateDetail',
 		type: 'POST',
@@ -164,9 +181,11 @@ function getcateDetail(ctx,cate_id){
 }
 
 //메뉴삭제 ajax
-function cateDelete(ctx,list){
+function cateDelete(ctx,list)
+{
 	var okFlag;
 	var data = JSON.stringify(list);
+	
 	$.ajaxSettings.traditional = true;
 	$.ajax({
 		url: ctx+'/cateDelete',
@@ -187,86 +206,121 @@ function cateDelete(ctx,list){
 }
 
 //버튼 이벤트
-function cateButtonEvent(ctx){	
+function cateButtonEvent(ctx)
+{	
 	//추가버튼 누를시 메뉴추가
 	$('#cateAddBtn').click(function(){
 		//초기화면
-		if($('#cate_mode').val() == 'waiting'){
-			if(cate_checkCount('masterCheck') == 0){	//체크가 없는 경우 상위메뉴 추가
+		if($('#cate_mode').val() == 'waiting')
+		{
+			if(cate_checkCount('masterCheck') == 0)		//체크가 없는 경우 상위메뉴 추가
+			{
 				$('#cate_mode').val('insert');
-				if(!($('#tmpText').is(':focus'))){
+				
+				if(!($('#tmpText').is(':focus')))
+				{
 					var topNode = $('#cateTree');
+					
 					topNode.append(
 							'<li><img class="no_cateFlag" src="'+ctx+'/resources/image/no_treebtn.png"/>'+
 							' <input type="checkbox" class="masterCheck">'+
 							' <a class="master_cate">'+
 							' <input id="tmpText" type="text" size="5" value="NewNode"></a></li>'
 					);
+					
 					var lastChildeNode = $(topNode).children().last();
+					
 					$(lastChildeNode).children('input[type="checkbox"]').prop('checked',true);
 					$('#tmpText').focus();
 					$('#tmpText').blur(function(){
 						var hascateName = validatecateName(ctx, $(this).val());
 
-						if(hascateName == 'true'){	//메뉴이름이 존재하면
+						if(hascateName == 'true')		//메뉴이름이 존재하면
+						{
 							alert("중복되는 메뉴명입니다.\n다시 입력해주세요.");
 							$(this).focus();
-						}else{
+						}
+						else
+						{
 							$('#cateSubmitBtn').show();
 							$('#cateResetBtn').show();
 							$('#up_cate_nm').prop('disabled',true);
 							$('#cateTree').find('input[type="checkbox"]').prop('disabled',true);
+							
 							//a태그에 텍스트 입력정보 넣기
-							if($(this).val() != ''){
+							if($(this).val() != '')
+							{
 								$(this).parent().text($(this).val());
-							}else{
+							}
+							else
+							{
 								$(this).parent().text('NewNode');
 							}
+							
 							$('#cate_nm').val($(cate_checkedNode('masterCheck')).next().text());
 							$('#act_y').prop('checked',true);
 							$(this).remove();
 						}
 					});
 				}
-			}else if(cate_checkCount('masterCheck') == 1){	//상위메뉴가 체크될 경우 하위메뉴 추가
+			}
+			else if(cate_checkCount('masterCheck') == 1)		//상위메뉴가 체크될 경우 하위메뉴 추가
+			{
 				$('#cate_mode').val('insert');
-				if(!($('#tmpText').is(':focus'))){
+				
+				if(!($('#tmpText').is(':focus')))
+				{
 					var node = $(cate_checkedNode('masterCheck')).parent();
 					var hasElement = $(node).find('ul').size(); //ul태그 여부 (하위 서브메뉴틀 없을경우)
 					
-					if(hasElement == 0){
+					if(hasElement == 0)
+					{
 						$(node).append(
 							'<ul class="catetree_sub">'+					
 							'<li><input type="checkbox" class="subCheck">'+
 							' <a class="sub_cate">'+
 							' <input id="tmpText" type="text" size="5" value="NewNode"/></a></li></ul>'
 						);
+						
 						$(node).find('.subCheck').prop('checked',false);
 						$(node).find('.subCheck').last().prop('checked',true);
 						$(node).find('li').show();
-						if($(node).children('.cateFlag').size() == 0 ){
+						
+						if($(node).children('.cateFlag').size() == 0 )
+						{
 							$(node).children('.no_cateFlag').remove();
 							$(node).prepend('<img class="cateFlag" src="'+ctx+'/resources/image/treebtn2.png"/>');
-						}else{
+						}
+						else
+						{
 							$(node).children('.cateFlag').attr('src',ctx+"/resources/image/treebtn2.png");
 						}
+						
 						$('#tmpText').focus();
 						$('#tmpText').blur(function(){
 							var hascateName = validatecateName(ctx, $(this).val());
 							
-							if(hascateName == 'true'){	//메뉴이름이 존재하면
+							if(hascateName == 'true')		//메뉴이름이 존재하면
+							{
 								alert("중복되는 메뉴명입니다.\n다시 입력해주세요.");
 								$(this).focus();
-							}else{
+							}
+							else
+							{
 								$('#cateSubmitBtn').show();
 								$('#cateResetBtn').show();
 								$('#cateTree').find('input[type="checkbox"]').prop('disabled',true);
+								
 								//a태그에 텍스트 입력정보 넣기
-								if($(this).val() != ''){
+								if($(this).val() != '')
+								{
 									$(this).parent().text($(this).val());
-								}else{
+								}
+								else
+								{
 									$(this).parent().text('NewNode');
 								}
+								
 								$('#cate_nm').val($(cate_checkedNode('subCheck')).next().text());
 								$('#up_cate_id').val($(cate_checkedNode('masterCheck')).val());
 								$('#up_cate_nm').val($(cate_checkedNode('masterCheck')).next().text());
@@ -274,12 +328,15 @@ function cateButtonEvent(ctx){
 								$(this).remove();	//임시로 만든 input text "tmpText"삭제
 							}
 						});
-					}else{
+					}
+					else
+					{
 						$(node).find('ul').append(				
 							'<li><input type="checkbox" class="subCheck">'+
 							' <a class="sub_cate">'+
 							' <input id="tmpText" type="text" size="5" value="NewNode"/></a></li>'
 						);
+						
 						$(node).find('.subCheck').prop('checked',false);
 						$(node).find('.subCheck').last().prop('checked',true);
 						$(node).find('li').show();
@@ -288,19 +345,27 @@ function cateButtonEvent(ctx){
 						$('#tmpText').blur(function(){
 							var hascateName = validatecateName(ctx, $(this).val());
 							
-							if(hascateName == 'true'){	//메뉴이름이 존재하면
+							if(hascateName == 'true')		//메뉴이름이 존재하면
+							{
 								alert("중복되는 메뉴명입니다.\n다시 입력해주세요.");
 								$(this).focus();
-							}else{
+							}
+							else
+							{
 								$('#cateSubmitBtn').show();
 								$('#cateResetBtn').show();
 								$('#cateTree').find('input[type="checkbox"]').prop('disabled',true);
+								
 								//a태그에 텍스트 입력정보 넣기
-								if($(this).val() != ''){
+								if($(this).val() != '')
+								{
 									$(this).parent().text($(this).val());
-								}else{
+								}
+								else
+								{
 									$(this).parent().text('NewNode');
 								}
+								
 								$('#cate_nm').val($(cate_checkedNode('subCheck')).next().text());
 								$('#up_cate_id').val($(cate_checkedNode('masterCheck')).val());
 								$('#up_cate_nm').val($(cate_checkedNode('masterCheck')).next().text());
@@ -310,10 +375,14 @@ function cateButtonEvent(ctx){
 						});
 					}
 				}
-			}else{
+			}
+			else
+			{
 				alert('추가를 하려면 하나만 선택하세요!');
 			}
-		}else{
+		}
+		else
+		{
 			alert('입력이 완료되지 않은 상태입니다!\n 정보를 입력하거나 취소버튼을 눌러주세요.');
 		}
 	});
@@ -321,47 +390,67 @@ function cateButtonEvent(ctx){
 	//삭제버튼 누를시 메뉴정보 삭제
 	$('#cateDelBtn').click(function(){
 		//초기화면
-		if($('#cate_mode').val() == 'waiting'){
-			if(cate_totalCheckCount() == 0){	//체크가 없는 경우
+		if($('#cate_mode').val() == 'waiting')
+		{
+			if(cate_totalCheckCount() == 0)		//체크가 없는 경우
+			{
 				alert('삭제할 대상을 선택해주세요.');
-			}else{
+			}
+			else
+			{
 				var result = confirm("선택한 항목을 삭제하시겠습니까?");
-				if(result){
+				
+				if(result)
+				{
 					var data = new Object();
 					var master = cate_checkedCateIDList("masterCheck");
 					var sub = cate_checkedCateIDList("subCheck");
+					
 					data.master = master;
 					data.sub = sub;
 					
 					var okFlag = cateDelete(ctx,data);
-					if(okFlag == 1){
+					
+					if(okFlag == 1)
+					{
 						alert('삭제되었습니다.');
 						var master = cate_checkNodeList('masterCheck');
-						for(var i=0; i<$(master).size(); i++){
+						
+						for(var i=0; i<$(master).size(); i++)
+						{
 							$(master[i]).next().css('text-decoration','line-through');
 							$(master[i]).prop('checked',false);
 						}
 						
 						var sub = cate_checkNodeList('subCheck');
-						for(var j=0; j<$(sub).size(); j++){
+						
+						for(var j=0; j<$(sub).size(); j++)
+						{
 							$(sub[j]).prop('checked',false);
 							$(sub[j]).next().css('text-decoration','line-through');
 						}
-					}else{
+					}
+					else
+					{
 						alert('올바르게 삭제되지 않았습니다.');
 					}
 				}
 			}
-		}else{
+		}
+		else
+		{
 			alert('입력이 완료되지 않은 상태입니다!\n 정보를 입력하거나 취소버튼을 눌러주세요.');
 		}
 	});
 	
 	//선택취소 버튼 누를시 체크박스 전체해제
 	$('#cateAllCancelBtn').click(function(){
-		if($('#cate_mode').val() == 'waiting'){
+		if($('#cate_mode').val() == 'waiting')
+		{
 			$('#cateTree').find('input').prop('checked',false);
-		}else{
+		}
+		else
+		{
 			alert('입력이 완료되지 않은 상태입니다!\n 정보를 입력하거나 취소버튼을 눌러주세요.');
 		}
 	});
@@ -371,37 +460,52 @@ function cateButtonEvent(ctx){
 
 
 //각 메뉴의 a태그를 누를시 이벤트
-function cateAnckEvent(ctx){
+function cateAnckEvent(ctx)
+{
 		//상위메뉴의 a태그 누를시
 		$('#cateTree').delegate('.master_cate','click',function(){
-			if($('#cate_mode').val() == 'waiting'){
+			if($('#cate_mode').val() == 'waiting')
+			{
 				$('#cateTree').find('input[type="checkbox"]').prop('checked',false);
 				cateForm_disable(true);
 				var cate_id = $(this).prev().val();
 				var cate = getcateDetail(ctx, cate_id);
+				
 				$('#cateUpdateBtn').show();
 				$('#cateResetBtn').show();
+				
 				cateDetailSetting(cate);
-			}else if($('#cate_mode').val() == 'insert'){
+			}
+			else if($('#cate_mode').val() == 'insert')
+			{
 				alert('정보 입력중입니다.');
-			}else{
+			}
+			else
+			{
 				alert('수정중입니다.');
 			}
 		});
 		
 		//하위메뉴의 a태그 누를시
 		$('#cateTree').delegate('.sub_cate','click',function(){
-			if($('#cate_mode').val() == 'waiting'){
+			if($('#cate_mode').val() == 'waiting')
+			{
 				$('#cateTree').find('input[type="checkbox"]').prop('checked',false);
 				cateForm_disable(true);
 				var cate_id = $(this).prev().val();
 				var cate = getcateDetail(ctx, cate_id);
+				
 				$('#cateUpdateBtn').show();
 				$('#cateResetBtn').show();
+				
 				cateDetailSetting(cate);
-			}else if($('#cate_mode').val() == 'insert'){
+			}
+			else if($('#cate_mode').val() == 'insert')
+			{
 				alert('정보 입력중입니다.');
-			}else{
+			}
+			else
+			{
 				alert('수정중입니다.');
 			}
 		});
